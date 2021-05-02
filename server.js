6798b44320877ec1ee8a16bbbc14d6cf37b1436a
vsmarimuthu <vsmarimuthu@gmail.com>
2021-05-02 13:57:18 +0530

    Changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var request = require('request');
-const restService = express();
-var jsforce1 = require('jsforce');
+const config = require('./config.json');
 var jsforce = require('node-salesforce');
-var port = process.env.port || 3000;
+const restService = express();
+
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -14,34 +14,44 @@ restService.use(
 restService.use(bodyParser.json());
 
 
-restService.post("/getReports", function (req, res) {
-	console.log('I came here');
+restService.post("/getReports", function (req, mainres) {	
   var speech ='';;
   var access_token = '';
     try{
 var conn = new jsforce.Connection({
-	//oauth2 : {
-  // you can change loginUrl to connect to sandbox or prerelease env.
-  loginUrl : 'https://test.salesforce.com',
-   //clientId : '3MVG9N6eDmZRVJOlOiQKbAJuqgE348vvvD0qPKb2Uct9OkEfOCq8HIEUFB7vWGnELmmxqJqKa8cTZITu7El8I',
-   // clientSecret : '1B2C540127B7E1F64F19CBF2C443B73549DDF12498B29785D9D417947F8922F3',
-   // redirectUri : 'https://test.salesforce.com'
-	//}
+  loginUrl : config.salesforceLoginURL
 });
-var _request = {
-  url: '/services/data/v45.0/limits/recordCount?sObjects=Account,User',
-  method: 'get',
-  body: '',
-  headers : {
-          "Content-Type" : "application/json"
-      }
-};
-
-conn.login('marimuthu.s@cardinality.ai.prvpod', 'Thisaimugam5ya2qy1F3Gzp2KJp3JgHNtoQy', function(err, userInfo) {
+console.log('req :>> ', JSON.stringify(req.body.queryResult.parameters));
+conn.login(config.salesforceUserName, config.salesforcePassword+config.salesforceSecurityToken, function(err, userInfo) {
 	console.log(conn.accessToken);
-  var body = { title: 'hello', num : 1 };
+  var body = { reportName: req.body.queryResult.parameters.reportName,
+              filterField: req.body.queryResult.parameters.filterField,
+              filterValue: req.body.queryResult.parameters.filterValue,
+              filteroperator: req.body.queryResult.parameters.filteroperator};
 conn.apex.post("/showreport/", body, function(res,respo) {
   console.log(respo);
+  speech = '<speak><break strength="x-strong"/> Congratulations. <break time=".5s"/>'+respo+' <say-as interpret-as="cardinal">' + speech + '</say-as></speak>';
+       var speechResponse = {
+        google: {
+         expectUserResponse: true,
+           richResponse: {
+           items: [{
+            simpleResponse: {
+                          textToSpeech: speech
+                                          }
+                                  }]
+                              }
+                            }
+                          };
+
+      return mainres.json({
+        payload: speechResponse,
+        //data: speechResponse,
+        fulfillmentText: speech,
+        speech: speech,
+        displayText: speech,
+        source: "Reports"
+    });
   // the response object structure depends on the definition of apex class
 });
  
@@ -57,7 +67,7 @@ conn.apex.post("/showreport/", body, function(res,respo) {
 });
 
 restService.listen(process.env.PORT || 5000, function () {
-  console.log("Server up and listening on port ",port );
+  console.log("Server up and listening on port");
 });
 
 //Body parser
