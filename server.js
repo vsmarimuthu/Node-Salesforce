c4d96b83b0c535d78765e0caa6abf7372e2179ea
MarimuthuVS <99012210+MarimuthuVS@users.noreply.github.com>
2022-07-07 17:24:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,24 +21,16 @@ restService.post("/getReports", function (req, mainres) {
 var conn = new jsforce.Connection({
   loginUrl : config.salesforceLoginURL
 });
-console.log('req :>> ', JSON.stringify(req.body.queryResult.parameters));
+//console.log('req :>> ', JSON.stringify(req.body.queryResult.parameters));
 conn.login(config.salesforceUserName, config.salesforcePassword+config.salesforceSecurityToken, function(err, userInfo) {
 	console.log(conn.accessToken);
- 
-  let _filter={column: req.body.queryResult.parameters.filterField,
-    value: req.body.queryResult.parameters.filterValue.toUTCString(),
-    operator: req.body.queryResult.parameters.filteroperator};
-    console.log('_filter :>> ', JSON.stringify(_filter));
-  var body = { Name: req.body.queryResult.parameters.reportName,
-              Filter : _filter
-              };
-              console.log('_filter :>> ', JSON.stringify(_filter));
-conn.apex.post("/createteslaquote/",JSON.stringify(body), function(res,respo) {
+
+conn.apex.post("/createteslaquote/", function(res,respo) {
   console.log(respo);
  
 
       return mainres.json({
-        payload: 'Success'
+        payload: conn.accessToken
     });
   // the response object structure depends on the definition of apex class
 });
