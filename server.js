13f91457bab4376fccffd7f6d77db5ea41253bc8
vsmarimuthu <vsmarimuthu@gmail.com>
2023-01-06 22:05:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,48 +14,23 @@ restService.use(
 restService.use(bodyParser.json());
 
 
-restService.post("/getReports", function (req, mainres) {	
+restService.post("/createQuote", function (req, mainres) {	
   var speech ='';;
   var access_token = '';
     try{
 var conn = new jsforce.Connection({
   loginUrl : config.salesforceLoginURL
 });
-console.log('req :>> ', JSON.stringify(req.body.queryResult.parameters));
+console.log('req :>> ', req.body);
 conn.login(config.salesforceUserName, config.salesforcePassword+config.salesforceSecurityToken, function(err, userInfo) {
 	console.log(conn.accessToken);
- 
-  let _filter={column: req.body.queryResult.parameters.filterField,
-    value: req.body.queryResult.parameters.filterValue.toUTCString(),
-    operator: req.body.queryResult.parameters.filteroperator};
-    console.log('_filter :>> ', JSON.stringify(_filter));
-  var body = { Name: req.body.queryResult.parameters.reportName,
-              Filter : _filter
-              };
-              console.log('_filter :>> ', JSON.stringify(_filter));
-conn.apex.post("/showreport/",JSON.stringify(body), function(res,respo) {
+
+conn.apex.post("/createteslaquote/",req.body, function(res,respo) {
   console.log(respo);
-  speech = '<speak><break strength="x-strong"/> Congratulations. <break time=".5s"/>'+respo+' <say-as interpret-as="cardinal">' + speech + '</say-as></speak>';
-       var speechResponse = {
-        google: {
-         expectUserResponse: true,
-           richResponse: {
-           items: [{
-            simpleResponse: {
-                          textToSpeech: speech
-                                          }
-                                  }]
-                              }
-                            }
-                          };
+ 
 
       return mainres.json({
-        payload: speechResponse,
-        //data: speechResponse,
-        fulfillmentText: speech,
-        speech: speech,
-        displayText: speech,
-        source: "Reports"
+        payload: conn.accessToken
     });
   // the response object structure depends on the definition of apex class
 });
